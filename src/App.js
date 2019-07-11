8add1b3b89aa31119eb68208630a72ba333bcf08
Mitchell Lopez <mitchellslopez@gmail.com>
2019-07-11 16:58:22 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import team from "./Components/team";
+import week from "./Components/week";
+import favorites from "./Components/favorites";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    current: "week"
+  };
+
+  renderContent() {
+    if (this.state.current === "week") {
+      return <week />;
+    }
+    if (this.state.current === "team") {
+      return <team />;
+    }
+    if (this.state.current === "favorites") {
+      return <favorites />;
+    }
+    this.setState({ current: "week" });
+  }
+
+  render() {
+    return <div className="content">{this.renderContent()}</div>;
+  }
 }
-
 export default App;
